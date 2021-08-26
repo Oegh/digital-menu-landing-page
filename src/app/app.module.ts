3e167d9e5ec4454e075d9d7367409514c8726fb2
danibautista410 <danibautista410@gmail.com>
2021-08-25 21:41:14 -0400

    How it works? section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { TargetComponent } from './components/target/target.component';
 import { FeaturesComponent } from './components/features/features.component';
+import { VideoTutorialComponent } from './components/video-tutorial/video-tutorial.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import { FeaturesComponent } from './components/features/features.component';
     NavbarComponent,
     IntroComponent,
     TargetComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    VideoTutorialComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/video-tutorial/video-tutorial.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import GLightbox from 'glightbox';
+import "@lottiefiles/lottie-player";
+@Component({
+  selector: 'app-video-tutorial',
+  templateUrl: './video-tutorial.component.html',
+  styleUrls: ['./video-tutorial.component.scss']
+})
+export class VideoTutorialComponent implements OnInit {
+
+  lightbox: any;
+
+  constructor() { }
+  
+  ngOnInit(): void {
+
+    this.lightbox = GLightbox({
+      'href': '../../../assets/video/pexels-lisa-7934113.mp4',
+      'type': 'video',
+      'source': 'local', //vimeo, youtube or local
+      'width': 900,
+      'autoplayVideos': true,
+      'touchNavigation': false,
+    });
+  }
+
+}
