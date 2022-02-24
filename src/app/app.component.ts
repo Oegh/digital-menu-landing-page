786f686a507b8d66982177e299887c69754c4266
Osqui <oegh1296@gmail.com>
2022-02-23 21:21:46 -0500

    initialize cookie consent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import * as AOS from 'aos';
 
+declare var cookieconsent: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,5 +12,21 @@ export class AppComponent {
 
   ngOnInit(){
     AOS.init();
+    cookieconsent.initialise({
+      palette: {
+          popup: {
+              background: '#3c404d',
+              text: '#d6d6d6'
+          },
+          button: {
+              background: '#66CC98'
+          }
+      },
+      'cookie.domain': 'https://menucodi.com',
+      theme: 'classic',
+      content: {
+          href: 'https://sites.google.com/view/digitalmenucookies'
+      }
+    });
     }
 }
